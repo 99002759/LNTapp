aa414d5a8a40284883d06aec916bf7a803b65d77
stepin104809 <37738211+stepin104809@users.noreply.github.com>
2020-11-09 16:59:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lntapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lntapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        Toast.makeText(this, "Hiii Loki Coder", Toast.LENGTH_SHORT).show();
+    }
+}
